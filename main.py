e6086ec822254f16b4af7b544bd3928ca4deb50b
Kael <kael.kill@khomp.com>
2023-06-23 17:36:43 -0300

    Change request to get json file and add language filter

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 import wget
+import json
 
 def main(): 
-    url = 'https://librivox.org/api/feed/audiobooks'
-    page = BeautifulSoup(requests.get(url).content)
+    offset = 0
+    url = 'https://librivox.org/api/feed/audiobooks/?format=json&fields={title,language,url_zip_file}&limit=5000&&offset=0'
+    page = requests.get(url, 'json')
 
-    books = page.xml.books
+    books = json.loads(page.text)
     
-    for book in books.contents:
-        # print(book.url_zip_file)
-        url = book.contents[8].contents[0]
-        print(url)
-        wget.download(url)
+    for book in books['books']:
+        if(book['language'] == 'Portuguese'):
+            print(book['title'])
+            url = book['url_zip_file']
+            wget.download(url)
+
         
 if __name__== '__main__':
     main()
